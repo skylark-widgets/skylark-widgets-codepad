298dd7668ce071f3d865fe18d12276516c6ad5fd
hudaokeji <github@hudaokeji.com>
2021-08-02 16:34:09 +0900

    update

---
+++ dist/uncompressed/skylark-widgets-coder/main.js
@@ -0,0 +1,11 @@
+define([
+	"./Coder",
+	"./addons/edit/codemirror",
+	"./addons/edit/ace",
+	"./addons/general/console",
+	"./addons/general/play",
+	"./addons/general/render"
+],function(Coder){
+
+	return Coder;
+})
